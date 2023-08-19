d7ad655b67265ade94bc4b74c92674a45021d27b
Afif-Mac <afifmansib123@gmail.com>
2023-08-19 16:03:50 +0700

    hi

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,13 @@ import 'firebase/firestore';
 
 // Use your own configs!
 const app = firebase.initializeApp({
-  apiKey: 'AIzaSyCbDHjASrAGq_qQh9WNbWESnTUTHK6jZes',
-  authDomain: 'auction-site-89bd1.firebaseapp.com',
-  projectId: 'auction-site-89bd1',
-  storageBucket: 'auction-site-89bd1.appspot.com',
-  messagingSenderId: '731721532151',
-  appId: '1:731721532151:web:96b37ffdcc443018e449f2',
+  apiKey: "AIzaSyCDwt7tjJVKl8qbzFK9gmfbza-Kw9_3mj4",
+  authDomain: "auction-site-2a7f4.firebaseapp.com",
+  projectId: "auction-site-2a7f4",
+  storageBucket: "auction-site-2a7f4.appspot.com",
+  messagingSenderId: "688575223108",
+  appId: "1:688575223108:web:6f7a4128447f1fb28cf311",
+  measurementId: "G-HR4EX3TM4D"
 });
 
 export const timestamp = firebase.firestore.FieldValue.serverTimestamp;
